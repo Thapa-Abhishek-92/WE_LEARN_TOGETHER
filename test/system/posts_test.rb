8514953f5b16509990c27b789ccb30cfe458593b
thapa abhishek <thapaabhishek9843@gmail.com>
2025-10-01 15:08:27 +0900

    fix: Update system tests for MVP (index only implementation)
    - Comment out update/destroy tests in posts_test.rb
    - Keep only index and show related tests
    - All tests now passing for current MVP scope

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -7,39 +7,30 @@ class PostsTest < ApplicationSystemTestCase
 
   test "visiting the index" do
     visit posts_url
-    assert_selector "h1", text: "Posts"
+    assert_selector "h1", text: "投稿一覧"
   end
 
-  test "should create post" do
+  test "should show post" do
     visit posts_url
-    click_on "New post"
-
-    fill_in "Content", with: @post.content
-    fill_in "Situation", with: @post.situation
-    fill_in "Title", with: @post.title
-    click_on "Create Post"
-
-    assert_text "Post was successfully created"
-    click_on "Back"
+    click_on "Show this post", match: :first
+    assert_selector "h1", text: "投稿詳細"
   end
 
-  test "should update Post" do
-    visit post_url(@post)
-    click_on "Edit this post", match: :first
+  # update と destroy のテストを削除またはコメントアウト
+  # test "should update Post" do
+  #   visit post_url(@post)
+  #   click_on "Edit this post", match: :first
+  #   fill_in "Content", with: @post.content
+  #   fill_in "Situation", with: @post.situation
+  #   fill_in "Title", with: @post.title
+  #   click_on "Update Post"
+  #   assert_text "Post was successfully updated"
+  #   click_on "Back"
+  # end
 
-    fill_in "Content", with: @post.content
-    fill_in "Situation", with: @post.situation
-    fill_in "Title", with: @post.title
-    click_on "Update Post"
-
-    assert_text "Post was successfully updated"
-    click_on "Back"
-  end
-
-  test "should destroy Post" do
-    visit post_url(@post)
-    click_on "Destroy this post", match: :first
-
-    assert_text "Post was successfully destroyed"
-  end
+  # test "should destroy Post" do
+  #   visit post_url(@post)
+  #   click_on "Destroy this post", match: :first
+  #   assert_text "Post was successfully destroyed"
+  # end
 end
