7f8d0ef7c0a85889e10546b42d56fd6ab5012a92
thapa abhishek <thapaabhishek9843@gmail.com>
2025-10-29 11:26:02 +0900

    Add Japanese translation for Devise messages and fix login flash display

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,12 @@
+# test/application_system_test_case.rb
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  # JS不要の通常画面テストは rack_test を使う（Chrome不要で高速＆安定）
+  driven_by :rack_test
+
+  # 将来、JSが必要なテストを書きたいときは個別テストで下記のように切り替え可:
+  #
+  # test "something with JS", js: true do
+  # end
 end
